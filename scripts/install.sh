0544caf0e17ab138a271f7124f598840544df9e7
hal2001so <hal2001so@outlook.com>
2016-07-29 08:56:53 -0400

    correct spacing in if

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,11 +2,11 @@
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 yum -y install nodejs && sudo yum -y install nginx
 service nginx stop
-if [ -f /etc/nginx/nginx.conf]
+if [ -f /etc/nginx/nginx.conf ]
 then
   rm /etc/nginx/nginx.conf
 fi
-sudo $pid=$(ps -ef | grep nohup | grep static | awk '{print $2}')
+pid=$(ps -ef | grep nohup | grep static | awk '{print $2}')
 if [ -n "$pid"]
 then
   kill $pid
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo service nginx start
+service nginx start
 pid=$(ps -ef | grep staticserver.js | awk '{print $2}')
-if [ -z "$pid"]
+if [ -z "$pid" ]
 then
   nohup node /usr/share/nginx/html/staticserver.js &
 fi
