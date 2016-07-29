edd431282427e7be7695610dd16a086aa5303a1a
hal2001so <hal2001so@outlook.com>
2016-07-29 07:07:51 -0400

    updated with kill pid

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,6 @@ curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 sudo yum -y install nodejs && sudo yum -y install nginx
 sudo service nginx stop
 sudo rm /etc/nginx/nginx.conf
+sudo $pid=$(ps -ef | grep nohup | grep static | awk '{print $2}')
+echo $pid
+sudo kill $pid
