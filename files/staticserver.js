5669e7f8d2294156626bb47d2070c86d4ef06390
hal2001so <hal2001so@outlook.com>
2016-07-29 11:23:02 -0400

    Changed staticserver.js to say 'hello david'

--- files/staticserver.js
+++ files/staticserver.js
@@ -6,7 +6,7 @@ var http = require("http"),                           //Import Node
               
 http.createServer(function(request, response) {       //Create server 
 response.writeHead(200)
-response.write('Hello Dusan!\n')
+response.write('Hello David!\n')
 response.end()
 }).listen(8081)                                     //Listening port 
 console.log("Server is listening on port 8081.")      //Terminal output
